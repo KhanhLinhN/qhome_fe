898bead63240359651f16af6ba7b5a7d22fbc70a
KhanhLinhN <Twilightspaker@gmail.com>
2025-10-23 12:27:55 +0700

    fix layout

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -8,7 +8,7 @@ export default function MainLayout({children}:{children:React.ReactNode}) {
       {/* Sidebar trái */}
       <Sidebar />
       {/* Nội dung + Topbar */}
-      <div className="flex-1 flex flex-col">
+      <div className="flex-1 flex flex-col md:ml-60">
         <Topbar />
         <main className="p-4 md:p-6">
           <div className="max-w-[1280px] mx-auto">{children}</div>
